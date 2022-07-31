7ccf75de166296fcd4b3886a772d06ac63a986ba
scirelli <scirelli+git@gmail.com>
2022-07-31 08:09:10 -0400

    Clean up test script a little

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,20 +4,41 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "$SCRIPT_DIR/variables.sh"
 source "$SCRIPT_DIR/utils.sh"
 
-clearDisplay
+test=0
 
-echo 'Test 1'
+function nextTest(){
+    sleep 2
+    clearDisplay
+    test=$((test + 1))
+}
+
+
+nextTest
+
+
+echo "Test $test"
+echo 'All red'
+setAll 255 0 0
+
+
+nextTest
+
+
+echo "Test $test"
+echo 'Segment test'
 echo '1=RED, 2=GREEN, 3=BLUE'
 echo "121 255 0 0" > "$PRU_SYSFS"
 echo "122 0 255 0" > "$PRU_SYSFS"
 echo "123 0 0 255" > "$PRU_SYSFS"
 echo -1 0 0 0 > "$PRU_SYSFS"
 
-echo 'Transition To'
-echo '1=BLUE, 2=RED, 3=GREEN'
+echo 'Transition To: 1=BLUE, 2=RED, 3=GREEN'
 echo 'After 1 second'
 sleep 1
 echo "121 0 0 255" > "$PRU_SYSFS"
 echo "122 255 0 0" > "$PRU_SYSFS"
 echo "123 0 255 0" > "$PRU_SYSFS"
 echo -1 0 0 0 > "$PRU_SYSFS"
+
+
+nextTest
