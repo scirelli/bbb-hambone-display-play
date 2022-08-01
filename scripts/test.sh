99f260d4ee5762aea26bc59b424b1929840e04f0
scirelli <scirelli+git@gmail.com>
2022-08-01 12:41:17 -0400

    test organization change

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,17 +4,22 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "$SCRIPT_DIR/utils.sh"
 
 DELAY_BETWEEN_TESTS=5
-test=0
+testNo=0
+
+function incTest(){
+    testNo=$((testNo + 1))
+    echo "$testNo"
+}
 
 function nextTest(){
     local delay="${1:-$DELAY_BETWEEN_TESTS}"
+    local testNo=$1
     sleep "$delay"
     clearDisplay
-    test=$((test + 1))
     echo
     echo
     echo
-    echo "Test $test"
+    echo "Test $testNo"
 }
 
 function end(){
@@ -23,61 +28,85 @@ function end(){
     clearDisplay
 }
 
+declare -a TESTS
+
+TESTS=("${TESTS[@]}" "test_${incTest}")
+function test_1() {
+    nextTest 1
+    echo 'All red'
+    setAndDrawAll 255 0 0
+}
 
 
-nextTest 0
-echo 'All red'
-setAndDrawAll 255 0 0
+TESTS=("${TESTS[@]}" "test_${incTest}")
+function test_2() {
+    nextTest 2
+    echo 'Segment test'
+    echo 'Segment 1=RED'
+    setSegment 1 255 0 0
+    echo 'Segment 2=GREEN'
+    setSegment 2 0 255 0
+    echo 'Segment 3=BLUE'
+    setSegment 3 0 0 255
+    draw
+
+    echo 'Transition To: 1=BLUE, 2=RED, 3=GREEN'
+    echo 'After 1 second'
+    sleep 1
+    setSegment 1 0 0 255
+    setSegment 2 255 0 0
+    setSegment 3 0 255 0
+    draw
+}
 
 
-
-nextTest
-echo 'Segment test'
-echo 'Segment 1=RED'
-setSegment 1 255 0 0
-echo 'Segment 2=GREEN'
-setSegment 2 0 255 0
-echo 'Segment 3=BLUE'
-setSegment 3 0 0 255
-draw
-
-echo 'Transition To: 1=BLUE, 2=RED, 3=GREEN'
-echo 'After 1 second'
-sleep 1
-setSegment 1 0 0 255
-setSegment 2 255 0 0
-setSegment 3 0 255 0
-draw
-
-
-
-nextTest
-echo 'Test bounds'
-echo 'Fade first pixel to red'
-index=$((0 + LED_COUNT))
-echo "(0, $index) on"
-echo "$index 255 0 0" > "$PRU_SYSFS"
-draw
-echo 'Fade last pixel to red'
-index=$((LED_COUNT + (LED_COUNT - 1)))
-echo "($((LED_COUNT - 1)), $index) on"
-echo "$index 255 0 0" > "$PRU_SYSFS"
-draw
-
-
-
-nextTest
-echo 'User defined segments (UDS). UDS use fading.'
-for (( i=0; i<LED_COUNT; i++ )); do
-    index=$((i + LED_COUNT))
-    echo "($i, $index) on"
+TESTS=("${TESTS[@]}" "test_${incTest}")
+function test_3(){
+    nextTest 3
+    echo 'Test bounds'
+    echo 'Fade first pixel to red'
+    index=$((0 + LED_COUNT))
+    echo "(0, $index) on"
     echo "$index 255 0 0" > "$PRU_SYSFS"
     draw
-    sleep 0.1
-    echo "($i, $index) off"
-    echo "$index 0 0 0" > "$PRU_SYSFS"
-done
+    echo 'Fade last pixel to red'
+    index=$((LED_COUNT + (LED_COUNT - 1)))
+    echo "($((LED_COUNT - 1)), $index) on"
+    echo "$index 255 0 0" > "$PRU_SYSFS"
+    draw
+}
+
+TESTS=("${TESTS[@]}" "test_${incTest}")
+function test_4(){
+    nextTest 4
+    echo 'User defined segments (UDS). UDS use fading.'
+    for (( i=0; i<LED_COUNT; i++ )); do
+        index=$((i + LED_COUNT))
+        echo "($i, $index) on"
+        echo "$index 255 0 0" > "$PRU_SYSFS"
+        draw
+        sleep 0.1
+        echo "($i, $index) off"
+        echo "$index 0 0 0" > "$PRU_SYSFS"
+    done
+}
 
 
+TESTS=("${TESTS[@]}" "test_${incTest}")
+function test_5() {
+    nextTest 5
+    echo 'Use user defined segment to blink last LED'
+    for (( i=0; i<10; i++ )); do
+        index=$(((LED_COUNT - 1) + LED_COUNT))
+        echo "$index on"
+        echo "$index 255 0 0" > "$PRU_SYSFS"
+        draw
+        sleep 0.1
+        echo "($index) off"
+        echo "$index 0 0 0" > "$PRU_SYSFS"
+    done
+}
+
+for i in "${!TESTS[@]}"; do   ${TESTS[$i]}; done
 
 end
